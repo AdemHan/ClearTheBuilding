dad2fa21c76bb67bfb8d0174b47ab274633eee04
Adem Han <adem.han@outlook.com>
2023-02-27 20:10:54 +0300

    Yumusak dondurme islemleri tamamlandi

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour
     Rigidbody rigidbody;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpPower = 13f;
+    [SerializeField] private float turnSpeed = 15f;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -23,28 +24,28 @@ public class Movement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))   //GetKeyDown-------> Sadece bir kez algýlar
         {                                      //GetKey-----------> Basýlý tutulduðu süre boyunca algýlar
 
-            rigidbody.velocity = new Vector3(rigidbody.velocity.x, (jumpPower * 100) * Time.deltaTime, 0f);                                   
-            print("jumping");
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, Mathf.Clamp((jumpPower * 100) * Time.deltaTime, 0f, 15f), 0f);                                   
+
         }
-        else
-        {
-            print("Not Jumping");
-        }
+
 
 
         if (Input.GetKey(KeyCode.A))            // if çalýþýyorsa else if çalýþmaz, else if çalýþýyorsa if çalýþmaz
         {
-            rigidbody.velocity = new Vector3((speed * 100) * Time.deltaTime, rigidbody.velocity.y, 0f);
-            print("Left");
+            rigidbody.velocity = new Vector3(Mathf.Clamp((speed * 100) * Time.deltaTime, 0f, 15f), rigidbody.velocity.y, 0f);
+            //transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, -180f, 0f), turnSpeed * Time.deltaTime);
+
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rigidbody.velocity = new Vector3((-speed * 100) * Time.deltaTime, rigidbody.velocity.y, 0f);
-            print("RÝght");
+            rigidbody.velocity = new Vector3(Mathf.Clamp((-speed * 100) * Time.deltaTime, -15f, 0f), rigidbody.velocity.y, 0f);
+            //transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), turnSpeed * Time.deltaTime);
         }
         else
         {
-            print("Not press A or D");
+            rigidbody.velocity = new Vector3(0f, rigidbody.velocity.y, 0f);
         }
     }
 }
