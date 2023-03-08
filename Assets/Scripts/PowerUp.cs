59d2540e0f01d8a03a0aa90aef255b653e39eb4b
Adem Han <adem.han@outlook.com>
2023-03-08 16:31:40 +0300

    Mathf.Sin ile nesnelere dönüş eklendi

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,9 +14,17 @@ public class PowerUp : MonoBehaviour
     public int ammoAmount = 5;
 
     [Header("Transform Settings")]
-    [SerializeField] private float turnSpeed = -1f;
+    [SerializeField] Vector3 turnVector = Vector3.zero;
+
+    [Header("Scale Settings")]
+    [SerializeField] private float period = 2f;
+    [SerializeField] Vector3 scaleVector;
+    [SerializeField] private float scaleFactor;
+    private Vector3 startScale;
     void Start()
     {
+        startScale = transform.localScale;
+
         if (healtPowerUp == true && ammoPowerUp == true)
         {
             healtPowerUp = false;
@@ -34,8 +43,29 @@ public class PowerUp : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0f, turnSpeed, 0f);
+        transform.Rotate(turnVector);
+        SinusWawe();
     }
+
+    private void SinusWawe()
+    {
+        if (period <= 0f)
+        {
+            period = 0.1f;
+        }
+        float cyles = Time.timeSinceLevelLoad / period;
+
+        const float piX2 = Mathf.PI * 2;
+
+        float sinusWawe = Mathf.Sin(cyles * piX2);
+
+        scaleFactor = sinusWawe / 2 + 0.5f; //-1 ve 1 aralýðýnda olmasýndansa 0 ve 1 aralýðýnda olmasýný saðladýk.
+
+        Vector3 offset = scaleFactor * scaleVector;
+
+        transform.localScale = startScale + offset;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
